ccf633969b526ac2fd03ac8c6629108f2463118d
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-03-16 14:06:07 -0700

    Updated links.go
    Added the ability to add links to the table. The ability to remove 
    them is still absent, and the code as written will probably crash if
    we try to add a link that already exists.

--- app/models/links.go
+++ app/models/links.go
@@ -5,14 +5,55 @@ import (
 )
 
 type TypeLink struct {
+	ID      int `json:"id"`
 	TypeID  int `json:"type_id"`
 	SpellID int `json:"spell_id"`
 }
 
 type SchoolLink struct {
+	ID       int `json:"id"`
 	SchoolID int `json:"school_id"`
 	SpellID  int `json:"spell_id"`
 }
+
+func InsertTypeLink(spellID int, typeID int) *TypeLink {
+	newLink = &TypeLink{SpellID: spellID, TypeID: typeID}
+
+	sqlString := `INSERT INTO type_links(type_id, spell_id) VALUES(?,?)`
+	res, err := app.Config.Database.Exec(sqlString, typeID, spellID)
+	if err != nil {
+		panic(err) //TODO: Proper error checking.
+	}
+	id, err := res.LastInsertId()
+		if err != nil {
+		panic(err) //TODO: Proper error checking.
+	}
+
+	newLink.ID = int(id)
+	return newLink
+}
+
+func InsertSchoolLink(spellID int, schoolID int) *SchoolLink {
+	newLink = &SchoolLink{SpellID: spellID, SchoolID: schoolID}
+
+	sqlString := `INSERT INTO school_links(school_id, spell_id) VALUES(?,?)`
+	res, err := app.Config.Database.Exec(sqlString, schoolID, spellID)
+	if err != nil {
+		panic(err) //TODO: Proper error checking.
+	}
+	id, err := res.LastInsertId()
+		if err != nil {
+		panic(err) //TODO: Proper error checking.
+	}
+
+	newLink.ID = int(id)
+	return newLink
+}
+
+
+//TODO: Add the ability to drop links.
+
+
 
 func init() {
 	gob.Register(TypeLink{})
