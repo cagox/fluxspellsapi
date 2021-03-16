107478dedb5bc6b3899c852b9b8da4119918c0e7
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-03-16 09:21:35 -0700

    Delete 000005_create_typelinks_table.up.sql

--- app/database/migrations/000005_create_typelinks_table.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS typelinks(
-    type_id INT NOT NULL,
-    spell_id INT NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES types (type_id) ON DELETE CASCADE,
-    FOREIGN KEY (spell_id) REFERENCES spells (spell_id) ON DELETE CASCADE,
-    PRIMARY KEY (type_id, spell_id)
-);
