f4a4de77a735544f5b6ceb47fe5ae925e84fd666
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-03-16 16:17:29 -0700

    Updated spell.go
    Pending testing, added code for adding and getting spells, as well as adding links to schools and types.

--- app/models/spell.go
+++ app/models/spell.go
@@ -17,19 +17,10 @@ type Spell struct {
 	Prerequisites string `json:"prerequisites"`
 	Components    string `json:"components"`
 	Range         string `json:"spellrange"`
-	//Types        Many-to-Many will be mapped as a function later.
-	//Schools      Many-to-Many will be mapped as a function later.
+	//Types()        Many-to-Many
+	//Schools()      Many-to-Many
 }
 
-
-
-func Types() {
-	//this is a stub. It will eventually return a list of types/categories associated with the spell.
-}
-
-func Schools() {
-	//This is a stub. It will eventually return a list of schools that the spell belongs in.
-}
 
 
 /*
@@ -40,7 +31,6 @@ type SpellSummary struct {
 	Name     string  `json:"name"`
 	Summary  string  `json:"summary"`
 }
-
 
 
 func (spell Spell) Link(classNames ...string) string {
@@ -54,13 +44,103 @@ func (spell SpellSummary) Link(classNames ...string) string {
 }
 
 
-func CreateAndInsertSpell(name string, description string, summary string, cost string, difficulty string, prerequisities string, components string spellRange string) {}
+func CreateAndInsertSpell(name string, description string, summary string, cost string, difficulty string, prerequisities string, components string spellRange string) * Spell {
+	spell := &Spell{Name: name, Description: description, Summary: summary, Cost: cost, Difficulty: difficulty, Prerequisites: prerequisites, Components: components, Range: spellRange}
+	return InsertSpell(spell)
+	//TODO: Add error checking.
+}
 
-func InsertSpell(spell *Spell) {}
+func InsertSpell(spell *Spell) *Spell {
+	sqlString := `INSERT INTO spells(?) VALUES(?,?,?,?,?,?,?,?)`
+	fields := `name, description, summary,cost, difficulty, prerequisites, components, range`
+	res, err := app.Config.Database.Exec(sqlString,fields, spell.Name, spell.Description, spell.Summary, spell.Cost, spell.Difficulty, spell.Prerequisites, spell.Components, spell.Range)
+	if err !=  nil {
+		panic(err) //TODO: More effective error checking.
+	}
+	
+	id, err := res.LastInsertId()
+		if err !=  nil {
+		panic(err) //TODO: More effective error checking.
+	}
+	
+	spell.ID = int(id)
+	return spell
+}
 
 
+func (spell Spell) Types() []Type {
+	spellTypes := make([]Type, 0)
+
+	sqlStatement := `SELECT ? FROM types INNER JOIN type_links ON types.type_id = type_links.type_id WHERE type_links.spell_id=?`
+	fields := `types.type_id, types.name, types.summary`
+	
+	rows, err := app.Config.Database.Query(sqlStatement, fields, spell.ID)
+		if err != nil {
+		//TODO: Figure out proper error checking and logging.
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		nextType := Type{}
+		if err := rows.Scan(&nextType.ID, &nextType.Name, &nextType.Summary); err != nil {
+			log.Fatal(err) //TODO: Figure out how to handle this properly
+		}
+		append(spellTypes, nextType)
+	}
+	return spellTypes
+}
+
+func (spell Spell) Schools() []School {
+	schools := make([]School, 0)
+
+	sqlStatement := `SELECT ? FROM schools INNER JOIN school_links ON schools.school_id = school_links.school_id WHERE school_links.spell_id=?`
+	fields := `schools.school_id, schools.name, schools.summary`
+	
+	rows, err := app.Config.Database.Query(sqlStatement, fields, spell.ID)
+		if err != nil {
+		//TODO: Figure out proper error checking and logging.
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		nextSchool := School{}
+		if err := rows.Scan(&nextSchool.ID, &nextSchool.Name, &nextSchool.Summary); err != nil {
+			log.Fatal(err) //TODO: Figure out how to handle this properly
+		}
+		append(schools, nextSchool)
+	}
+	return schools
+}
+
+func (spell Spell) AddType(typeID int) {
+	InsertTypeLink(spell.ID, typeID)
+}
 
 
+func (spell Spell) AddSchool(schoolID int) {
+	InsertSchoolLink(spell.ID, schoolID)
+}
+
+func GetSpellByID(id int) *Spell {
+	spell = new(Spell)
+	
+	sqlStatement := `SELECT ? FROM spells WHERE spell_id=?`
+	fields := `spell_id, name, description, summary, cost, difficulty, prerequisites, components, spellrange`
+	
+	row, err := app.Config.Database.QueryRow(sqlStatement, fields, id)
+	switch err := row.Scan(&spell.ID, &spell.Name, &spell.Description, &spell.Summary, &spell.Cost, &spell.Difficulty, &spell.Prerequisites, &spell.Components, &spell.Range); err {
+	case sql.ErrNoRows:
+		fmt.Println("Spell ID ", id,  "doesn't exist.")
+	case nil:
+		return spell
+	default:
+		panic(err)
+	}
+	return spell
+	//TODO: Fix error checking to something more useful for web dev.	
+}
 
 func init() {
 	gob.Register(Spell{})
