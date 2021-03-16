189666b919ce6ecf1a0bdfd1ed9140cb21250604
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-03-16 14:15:30 -0700

    Began Updating spell.go
    Still a lot to do here, but I have started implementing functionality.

--- app/models/spell.go
+++ app/models/spell.go
@@ -4,6 +4,9 @@ import (
 	"encoding/gob"
 )
 
+/*
+ * Spell is the full spell struct that will be used to display spell information.
+ */
 type Spell struct {
 	ID            int    `json:"spell_id"`
 	Name          string `json:"name"`
@@ -18,6 +21,8 @@ type Spell struct {
 	//Schools      Many-to-Many will be mapped as a function later.
 }
 
+
+
 func Types() {
 	//this is a stub. It will eventually return a list of types/categories associated with the spell.
 }
@@ -26,6 +31,38 @@ func Schools() {
 	//This is a stub. It will eventually return a list of schools that the spell belongs in.
 }
 
+
+/*
+ * SpellSummary is the struct that will be used to display brief information on index pages.
+ */
+type SpellSummary struct {
+	ID       int     `json:"spell_id"`
+	Name     string  `json:"name"`
+	Summary  string  `json:"summary"`
+}
+
+
+
+func (spell Spell) Link(classNames ...string) string {
+	classes := util.ClassString(classNames)
+	return fmt.Sprintf(`<a %s href="/spells/%d">%s</a>`, classes, spell.ID, spell.Name)
+}
+
+func (spell SpellSummary) Link(classNames ...string) string {
+	classes := util.ClassString(classNames)
+	return fmt.Sprintf(`<a %s href="/spells/%d">%s</a>`, classes, spell.ID, spell.Name)
+}
+
+
+func CreateAndInsertSpell(name string, description string, summary string, cost string, difficulty string, prerequisities string, components string spellRange string) {}
+
+func InsertSpell(spell *Spell) {}
+
+
+
+
+
 func init() {
 	gob.Register(Spell{})
+	gob.Register(SpellSummary{})
 }
