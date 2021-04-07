2b22e7835d77652bc3b395fa6a942b0e405537b7
Cagox Media <45616578+cagox@users.noreply.github.com>
2021-04-07 13:09:07 -0700

    Completed JSON routes.
    Added routes and handlers to return JSON for a given School, and the Spells associated with the school.

--- app/routes/schools.go
+++ app/routes/schools.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/cagox/fluxspellsapi/app"
 	"github.com/cagox/fluxspellsapi/app/models"
+	"github.com/gorilla/mux"
 	"net/http"
 )
 
@@ -15,6 +16,30 @@ func schoolsHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func schoolHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	school := School.GetSchoolByID(vars['school_id'])
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(school.ToJSON())
+}
+
+func schoolSpellsHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	school := School.GetSchoolByID(vars['school_id'])
+	
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(school.SpellsToJSON())
+}
+
+
+
 func init() {
 	app.Config.Router.HandleFunc("/api/schools/", schoolsHandler)
+	app.Conig.Router.HandleFunc("/api/schools/{school_id}", schoolHandler)
+	app.Conig.Router.HandleFunc("/api/schools/{school_id}/spells", schoolSpellHandler)
 }
+
+
