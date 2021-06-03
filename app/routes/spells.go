c07c4b6f535fc3885060a740f8523f2aee4366ab
burlingk <burlingk@gmail.com>
2021-06-02 21:28:16 -0700

    Added handler to return a single spell. /api/spells/{spell_id:[0-9]+} returns a spell.

--- app/routes/spells.go
+++ app/routes/spells.go
@@ -3,7 +3,9 @@ package routes
 import (
 	"github.com/cagox/fluxspellsapi/app"
 	"github.com/cagox/fluxspellsapi/app/models"
+	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 func spellsHandler(w http.ResponseWriter, r *http.Request) {
@@ -14,8 +16,26 @@ func spellsHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write(spells)
 }
 
+func spellHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id_val, err := strconv.Atoi(vars["spell_id"])
+	if err != nil {
+		panic(err)
+	}
+
+	spell := models.GetSpellByID(id_val)
+
+	spellJson := spell.ToJSON()
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Write(spellJson)
+}
 
 func init() {
 	app.Config.Router.HandleFunc("/api/spells/", spellsHandler)
 	app.Config.Router.HandleFunc("/api/spells", spellsHandler)
+	app.Config.Router.HandleFunc("/api/spells/{spell_id:[0-9]+}", spellHandler)
+	app.Config.Router.HandleFunc("/api/spells/{spell_id:[0-9]+}/", spellHandler)
 }
